25ecd3e3d9f54c5f6d12292e409029a8052e6b36
hieurury <hieurury007@gmail.com>
2025-03-15 20:59:22 +0700

    thêm header và chỉnh sửa trang chủ

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,12 +1,21 @@
 <template>
-    <header>
-        <nav>
-            <router-link to="/">Home</router-link>
-            <router-link to="/about">About</router-link>
-            <router-link to="/contact">Contact</router-link>
+    <header class="bg-black p-4">
+        <nav 
+        class="flex font-[Cabin] pb-2 text-2xl
+         text-amber-50 list-none font-semibold
+         border-b-white border-b">
+            <li class="mx-4">
+                <router-link to="/">Home</router-link>
+            </li>
+            <li class="mx-4">
+                <router-link to="/about">About</router-link>
+            </li>
+            <li class="mx-4">
+                <router-link to="/contact">Contact</router-link>
+            </li>
         </nav>
     </header>
-    <router-view>
+    <router-view class="bg-black min-h-screen p-4">
     </router-view>
 </template>
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,52 @@
 <template>
-    <div>
-        <h1>
-            Chào đằng ấy, Web vẫn đang xây hãy ghé thăm sau nhé!
-        </h1>
+    <div class="container">
+        <h1 class="text-white transition">{{ text }} <span id="space">{{ icon }}</span></h1>
     </div>
 </template>
 
 <script setup>
+    import { ref } from 'vue'
+    const says = ref([
+            "hi there! I'am hieurury",
+            "I'm a web developer",
+            "I'm a web designer",
+            "I'm studying at the University of Information Technology",
+    ])
+    function getRandomText(data) {
+        let text = data[0];
+        data.push(data[0]);
+        data.shift();
+        return text;
+    }
+    
+    const randomText = ref(getRandomText(says.value).split(''));
+    const text = ref('');
+    const icon = ref("✏️");
+    let isRun = true;
+   function startInterval(data) {
+        const setText = setInterval(() => {
+            if(data.length > 0 && isRun) {
+                icon.value = "✏️";
+                text.value += `${data[0]}`;
+                data.shift();
+            } else if(data.length === 0 && isRun) {
+                setTimeout(() => {
+                    isRun = false
+                }, 1000)
+            } else if(!isRun) {
+                icon.value = "📍";
+                text.value = text.value.slice(0, -1);
+                if(text.value.length === 0) {
+                    clearInterval(setText);
+                    data = getRandomText(says.value).split('');
+                    isRun = true;
+                    startInterval(data);
+                }
+            }
+    }, 50)
+   }
+    startInterval(randomText.value);
 </script>
 
 <style lang="css" scoped>
-
 </style>
