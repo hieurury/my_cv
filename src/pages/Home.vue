04d195e2a705a6c5a7204ec05e894dc8cd457884
hieurury <hieurury007@gmail.com>
2025-03-15 21:31:42 +0700

    cấu hình lại home với component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,51 +1,16 @@
 <template>
-    <div class="container">
-        <h1 class="text-white transition">{{ text }} <span id="space">{{ icon }}</span></h1>
+    <div class="container px-16 text-white">
+        <div class="flex justify-center items-center flex-col">
+            <h1 class="text-4xl font-semibold mb-2">
+                <span class="lg:inline-block block">Hi there!</span>
+                 I'm <span class="text-orange-400">hieurury</span></h1>
+            <Introduce />
+        </div>
     </div>
 </template>
 
 <script setup>
-    import { ref } from 'vue'
-    const says = ref([
-            "hi there! I'am hieurury",
-            "I'm a web developer",
-            "I'm a web designer",
-            "I'm studying at the University of Information Technology",
-    ])
-    function getRandomText(data) {
-        let text = data[0];
-        data.push(data[0]);
-        data.shift();
-        return text;
-    }
-    
-    const randomText = ref(getRandomText(says.value).split(''));
-    const text = ref('');
-    const icon = ref("✏️");
-    let isRun = true;
-   function startInterval(data) {
-        const setText = setInterval(() => {
-            if(data.length > 0 && isRun) {
-                icon.value = "✏️";
-                text.value += `${data[0]}`;
-                data.shift();
-            } else if(data.length === 0 && isRun) {
-                setTimeout(() => {
-                    isRun = false
-                }, 1000)
-            } else if(!isRun) {
-                icon.value = "📍";
-                text.value = text.value.slice(0, -1);
-                if(text.value.length === 0) {
-                    clearInterval(setText);
-                    data = getRandomText(says.value).split('');
-                    isRun = true;
-                    startInterval(data);
-                }
-            }
-    }, 50)
-   }
-    startInterval(randomText.value);
+    import Introduce from '../Components/Introduce.vue';
 </script>
 
 <style lang="css" scoped>
