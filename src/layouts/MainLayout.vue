f723b852915226d0a6a59418d2895965172b6e31
hieurury <hieurury007@gmail.com>
2025-03-15 21:33:39 +0700

    thay đổi 1 ít layout cho phù hợp

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,7 @@
         <nav 
         class="flex font-[Cabin] pb-2 text-2xl
          text-amber-50 list-none font-semibold
-         border-b-white border-b">
+         border-b-white border-b px-16">
             <li class="mx-4">
                 <router-link to="/">Home</router-link>
             </li>
